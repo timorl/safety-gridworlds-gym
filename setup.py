7942c8beb662841ab48684b136e07b462ac0dd3f
timorl <tymorl@gmail.com>
2018-11-05 16:34:33 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safety_gridworlds_gym",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=["numpy", "gym"],
+    license="",
+)
